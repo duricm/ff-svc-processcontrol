5b068360fafa71bedfc506128d425de50ff4f30e
Mehmed <duricm@hotmail.com>
2020-02-03 15:25:17 -0500

    Adding URL value to @CrossOrigin annotation to resolve front end error

--- src/main/java/com/bitcoin/card/BitcoinCardController.java
+++ src/main/java/com/bitcoin/card/BitcoinCardController.java
@@ -29,7 +29,7 @@ import java.util.Optional;
 
 import org.apache.log4j.Logger;
 
-@CrossOrigin()
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class BitcoinCardController {
 	
